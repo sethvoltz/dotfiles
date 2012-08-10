cb2c8cb988c8959a68326e00a96b7218412f6ee5
Seth Voltz <tauceti@designgods.net>
2012-08-10 14:36:16 -0700

    Turning off the DYLD_LIBRARY_PATH hack due to warnings
    It seems this is no longer safe under OS X 10.8 "Mountain Lion" and it also no longer seems to be necessary with the latest packages installed.

--- osx/dyld.zsh
+++ osx/dyld.zsh
@@ -1,2 +1,2 @@
 # Make sure dylibs are available for MySQL -- Path is for default MySQL and Homebrew instal
-export DYLD_LIBRARY_PATH=/usr/local/mysql/lib:$DYLD_LIBRARY_PATH
+# export DYLD_LIBRARY_PATH=/usr/local/mysql/lib:$DYLD_LIBRARY_PATH
