a4bb3e020cdb67fc21224c36788195f961aa3260
Seth Voltz <tauceti@designgods.net>
2013-08-07 13:56:27 -0700

    Added Java and EC2 envionment variables

---
+++ ec2/environment.zsh
@@ -0,0 +1,3 @@
+export EC2_PRIVATE_KEY="$(/bin/ls "$HOME"/.ec2/pk-*.pem | /usr/bin/head -1)"
+export EC2_CERT="$(/bin/ls "$HOME"/.ec2/cert-*.pem | /usr/bin/head -1)"
+export AWS_AUTO_SCALING_HOME="/usr/local/Library/LinkedKegs/auto-scaling/jars"
