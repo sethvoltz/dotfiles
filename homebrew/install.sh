c8a285cec3f6c7825e4b5746f306acc72705e51a
Seth Voltz <seth@designgods.net>
2015-04-06 13:11:21 -0700

    Updated homebrew install script to be smarter and clearer about installation requiring root access

--- homebrew/install.sh
+++ homebrew/install.sh
@@ -5,6 +5,10 @@
 # This installs some of the common dependencies needed (or at least desired)
 # using Homebrew.
 
+info () {
+  printf "  [ \033[00;34m..\033[0m ] $1"
+}
+
 # Check for Homebrew
 if test ! $(which brew)
 then
@@ -26,7 +30,12 @@ brew install htop-osx iftop mtr nmap p0f trafshow ngrep wget
 # brew install wireshark --with-x
 
 iftop_version=$(brew list --versions iftop | head -n 1 | cut -f 2 -d ' ')
-sudo chown root:wheel /usr/local/Cellar/iftop/$iftop_version/sbin/iftop
-sudo chmod u+s /usr/local/Cellar/iftop/$iftop_version/sbin/iftop
+
+if [[ $(ls -al /usr/local/Cellar/iftop/$iftop_version/sbin/iftop | cut -c4) != "s" ]]
+then
+  info 'Updating iftop to run set UID root, you may be asked for your password...'
+  sudo chown root:wheel /usr/local/Cellar/iftop/$iftop_version/sbin/iftop
+  sudo chmod u+s /usr/local/Cellar/iftop/$iftop_version/sbin/iftop
+fi
 
 exit 0
