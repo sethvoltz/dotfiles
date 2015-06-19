bac1a2e43987be85b1a7ab3964228d5bf33bc9ce
Seth Voltz <seth.voltz@relateiq.com>
2015-06-19 13:52:33 -0700

    fix(Git): Update git diff alias to include less call for pagination

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -3,7 +3,7 @@ alias gl='git pull --prune'
 alias glog="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
 alias gp='git push origin HEAD'
 # alias gd='git diff'
-alias gd='git difftool -y -x "colordiff -y -W $COLUMNS"'
+alias gd='git difftool -y -x "colordiff -y -W $COLUMNS" | less -rXF'
 alias gc='git commit'
 alias gca='git commit -a'
 alias gco='git checkout'
