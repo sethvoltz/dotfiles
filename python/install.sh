7d1b21db61960b9b2d74674ff6b82581fa8ec7c1
Seth Voltz <seth.voltz@relateiq.com>
2015-07-08 16:21:37 -0700

    feat(Python): Add Python 3 and pip3 to the list of available pythons installed

--- python/install.sh
+++ python/install.sh
@@ -5,3 +5,9 @@ then
   echo "  Installing homebrew Python for you."
   brew install python > /tmp/python-install.log
 fi
+
+if test ! $(which python | grep '/usr/local')
+then
+  echo "  Installing homebrew Python 3 for you."
+  brew install python3 > /tmp/python3-install.log
+fi
