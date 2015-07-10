faa282a8b04517cb3eb046142726bfea8ae871e4
Seth Voltz <seth.voltz@relateiq.com>
2015-07-10 11:36:19 -0700

    fix(Pianobar): Add growlnotify to install file

--- pianobar/install.sh
+++ pianobar/install.sh
@@ -9,3 +9,8 @@ if [ ! $(which pianobar) ]; then
   echo "  Installing rbenv for you."
   brew install pianobar > /tmp/pianobar-install.log
 fi
+
+if [ ! $(which growlnotify) ]; then
+  echo "  Installing growlnotify for you, this may require your password."
+  brew cask install growlnotify > /tmp/growlnotify-install.log
+fi
