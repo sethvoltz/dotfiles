3833fd95869b4dd129969ce29b02fea7886d5529
Seth Voltz <seth.voltz@relateiq.com>
2015-08-25 15:53:37 -0700

    feat(ZSH): Add check_age script for this repo to ensure it remains updated between machines

---
+++ zsh/check_age.zsh
@@ -0,0 +1,9 @@
+if [ $(which brew) ]; then
+  let head_age=`/usr/bin/stat -f "%m" $ZSH/.git/FETCH_HEAD 2> /dev/null || echo 0`
+  let repo_age=`date +"%s"`-$head_age
+  let grace_period="86400" # 86400 = 24 hours
+  if [ "$repo_age" -gt "$grace_period" ]; then
+    echo "ZSH repo is out of date (older than $grace_period seconds)... updating"
+    brew update
+  fi
+fi
