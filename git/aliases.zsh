59cef697bdff16e9a23f47b894f26c81653af1a5
Seth Voltz <seth.voltz@relateiq.com>
2016-01-15 13:02:47 -0800

    feat(Git): Add a force-with-lease push option to aliases

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -2,6 +2,8 @@
 alias gl='git pull --prune'
 alias glog="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
 alias gp='git push origin HEAD'
+alias gpu='git push --set-upstream origin $(git rev-parse --abbrev-ref HEAD)'
+alias gpfl='gp --force-with-lease'
 # alias gd='git difftool -y -x "colordiff -y -W $COLUMNS" | less -rXF'
 alias gd='git diff | cdiff -s -w 0'
 alias gc='git commit'
@@ -12,6 +14,5 @@ alias gs='git status -sb' # upgrade your git if -sb breaks for you. it's fun.
 alias grm="git status | grep deleted | awk '{print \$3}' | xargs git rm"
 alias gms="git merge origin/staging"
 alias grs="git rebase origin/staging"
-alias gpu='git push --set-upstream origin $(git rev-parse --abbrev-ref HEAD)'
 alias ga='git add'
 alias gaa='ga .'
