8ad4963e0c32e5bb738acebfb5262ce10160b48f
Seth Voltz <seth.voltz@relateiq.com>
2016-05-17 10:18:30 -0700

    feat(ZSH): Remove complete_aliases setopt to allow command param completion of aliases

--- zsh/config.zsh
+++ zsh/config.zsh
@@ -45,7 +45,8 @@ setopt HIST_REDUCE_BLANKS
 
 # don't expand aliases _before_ completion has finished
 #   like: git comm-[tab]
-setopt complete_aliases
+# Comment out to allow autocompletion of aliased command parameters
+# setopt complete_aliases
 
 zle -N newtab
 
