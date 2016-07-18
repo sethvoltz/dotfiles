22f1126505af421336b7a21b45bd7816cd093144
Seth Voltz <seth@designgods.net>
2016-07-17 23:36:18 -0700

    feat(Hammerspoon): Monitor-Monitor code more or less solid

--- hammerspoon/hammerspoon.symlink/monitor-monitor.lua
+++ hammerspoon/hammerspoon.symlink/monitor-monitor.lua
@@ -2,9 +2,32 @@
 function handleLayoutChange(layout)
   if layout then
     local screenId = hs.window.focusedWindow():screen():id()
+    setActiveScreen(screenId)
     print('current display ' .. screenId)
   end
 end
 
+function setActiveScreen(screenId)
+  local devicePath = getSerialOutputDevice()
+  if devicePath == "" then return end
+
+  local file = assert(io.open(devicePath, "w"))
+  file:write("set " .. screenId .. "\n")
+  file:flush()
+  file:close()
+end
+
+function getSerialOutputDevice()
+  local command = "ioreg -c IOSerialBSDClient -r -t " ..
+    "| awk 'f;/Photon/{f=1};/IOCalloutDevice/{exit}' " ..
+    "| sed -n 's/.*\"\\(\\/dev\\/.*\\)\".*/\\1/p'"
+  local handle = io.popen(command)
+  local result = handle:read("*a")
+  handle:close()
+  -- Strip whitespace - https://www.rosettacode.org/wiki/Strip_whitespace_from_a_string/Top_and_tail#Lua
+  return result:match("^%s*(.-)%s*$")
+end
+
 hs.screen.watcher.newWithActiveScreen(handleLayoutChange):start()
+handleLayoutChange(true) -- set the initial screen
 print('Monitor-Monitor loaded')
