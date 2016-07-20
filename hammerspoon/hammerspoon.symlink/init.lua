476bc943dbf17e8edc4d9088971bc53ab4061a4b
Seth Voltz <svoltz@salesforce.com>
2016-07-20 14:35:55 -0700

    feat(Hammerspoon): Add auto-loader for modules directory

--- hammerspoon/hammerspoon.symlink/init.lua
+++ hammerspoon/hammerspoon.symlink/init.lua
@@ -1,21 +1,19 @@
--- Tools by Topic
-require "modules/monitor-monitor"
-require "modules/caffeine"
-require "modules/audio-device"
-require "modules/screen-settings"
+util = require('util')
+util.autoimport("./modules")
 
 -- Fancy auto-reload thing
 function reloadConfig(files)
-    doReload = false
-    for _,file in pairs(files) do
-        if file:sub(-4) == ".lua" then
-            doReload = true
-        end
+  doReload = false
+  for _,file in pairs(files) do
+    if file:sub(-4) == ".lua" then
+      doReload = true
     end
-    if doReload then
-        hs.reload()
-    end
+  end
+  if doReload then
+    hs.reload()
+  end
 end
+
 hs.pathwatcher.new(os.getenv("HOME") .. "/.hammerspoon/", reloadConfig):start()
 hs.notify.new({
   title="Hammerspoon",
--- hammerspoon/hammerspoon.symlink/modules/screen-settings.lua
+++ hammerspoon/hammerspoon.symlink/modules/screen-settings.lua
@@ -3,11 +3,13 @@ local thunderboltBrightness = 0.7
 function handleLayoutChange()
   local thunderboltCount = 0
   local screenSet = hs.screen.allScreens()
+
   for _, screen in pairs(screenSet) do
     if screen:name():match("Thunderbolt Display") then
       thunderboltCount = thunderboltCount + 1
     end
   end
+
   if thunderboltCount >= 2 then
     for _, screen in pairs(screenSet) do
       screen:setBrightness(thunderboltBrightness)
