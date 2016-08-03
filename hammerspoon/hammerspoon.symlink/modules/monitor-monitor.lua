33da870f7d4a0193754738aa3c0577290d8d0ac2
Seth Voltz <seth@designgods.net>
2016-08-02 21:49:04 -0700

    feat(Hammerspoon): Add feature to show current display on-screen with virtual indicator

--- hammerspoon/hammerspoon.symlink/modules/monitor-monitor.lua
+++ hammerspoon/hammerspoon.symlink/modules/monitor-monitor.lua
@@ -1,20 +1,74 @@
-lastScreenId = -1
+local lastScreenId = -1
+local usePhysicalIndicator = true
+local useVirtualIndicator = true
 
--- Detect layout change
+local currentIndicator = nil
+local indicatorHeight = 3 -- 0..1 = percent of menubar, >1 = pixel height
+local indicatorColor = hs.drawing.color.asRGB({
+  red = 0,
+  green = 1.0,
+  blue = 0,
+  alpha = 0.3
+})
+
+-- -------------------------------------------------------= Change Handlers =--=
+
 function handleLayoutChange()
-  -- Always send the screenId, even on real layout changes (could be add/remove display)
   local focusedWindow = hs.window.focusedWindow()
   if not focusedWindow then return end
 
-  local screenId = focusedWindow:screen():id()
+  local screen = focusedWindow:screen()
+  local screenId = screen:id()
   if screenId == lastScreenId then return end
-
   lastScreenId = screenId
-  setActiveScreen(screenId)
-  print('current display ' .. screenId)
+
+  if usePhysicalIndicator then updatePhysicalScreenIndicator(screenId) end
+  if useVirtualIndicator  then updateVirtualScreenIndicator(screen) end
+  print('Display changed to ' .. screenId)
 end
 
-function setActiveScreen(screenId)
+
+-- ----------------------------------------------------= Virtual Indicators =--=
+
+function updateVirtualScreenIndicator(screen)
+  clearIndicator()
+
+  local screeng = screen:fullFrame()
+
+  if indicatorHeight >= 0.0 and indicatorHeight <= 1.0 then
+    height = indicatorHeight*(screen:frame().y - screeng.y)
+  else
+    height = indicatorHeight
+  end
+
+  indicator = hs.drawing.rectangle(hs.geometry.rect(
+    screeng.x,
+    screeng.y,
+    screeng.w,
+    height
+  ))
+
+  indicator:setFillColor(indicatorColor)
+    :setFill(true)
+    :setLevel(hs.drawing.windowLevels.overlay)
+    :setStroke(false)
+    :setBehavior(hs.drawing.windowBehaviors.canJoinAllSpaces)
+    :show()
+
+  currentIndicator = indicator
+end
+
+function clearIndicator()
+   if currentIndicator ~= nil then
+      currentIndicator:delete()
+      currentIndicator = nil
+   end
+end
+
+
+-- ---------------------------------------------------= Physical Indicators =--=
+
+function updatePhysicalScreenIndicator(screenId)
   local devicePath = getSerialOutputDevice()
   if devicePath == "" then return end
 
@@ -35,7 +89,9 @@ function getSerialOutputDevice()
   return result:match("^%s*(.-)%s*$")
 end
 
--- Watchers
+
+-- --------------------------------------------------------------= Watchers =--=
+
 hs.screen.watcher.newWithActiveScreen(handleLayoutChange):start()
 hs.spaces.watcher.new(handleLayoutChange):start()
 handleLayoutChange(true) -- set the initial screen
