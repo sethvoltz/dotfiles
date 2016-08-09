04585beaafe473757b93f3f84319e687aca73e34
Seth Voltz <svoltz@salesforce.com>
2016-08-09 09:46:54 -0700

    fix(Hammerspoon): Disable audio switcher, it causes unexpected audio splitting

--- hammerspoon/hammerspoon.symlink/modules/audio-device.lua
+++ hammerspoon/hammerspoon.symlink/modules/audio-device.lua
@@ -1,28 +1,28 @@
--- Define audio device names for headphone/speaker switching
-local usbAudioSearch = "VIA Technologies" -- USB sound card
-
--- Toggle between speaker and headphone sound devices (useful if you have multiple USB soundcards that are always connected)
-function setDefaultAudio()
-  local current = hs.audiodevice.defaultOutputDevice()
-  local usbAudio = findOutputByPartialUID(usbAudioSearch)
-
-  if usbAudio and current:name() ~= usbAudio:name() then
-    usbAudio:setDefaultOutputDevice()
-  end
-
-  hs.notify.new({
-    title='Hammerspoon',
-    informativeText='Default output device:' .. hs.audiodevice.defaultOutputDevice():name()
-  }):send()
-end
-
-function findOutputByPartialUID(uid)
-  for _, device in pairs(hs.audiodevice.allOutputDevices()) do
-    if device:uid():match(uid) then
-      return device
-    end
-  end
-end
-
-hs.usb.watcher.new(setDefaultAudio):start()
-setDefaultAudio()
+-- -- Define audio device names for headphone/speaker switching
+-- local usbAudioSearch = "VIA Technologies" -- USB sound card
+--
+-- -- Toggle between speaker and headphone sound devices (useful if you have multiple USB soundcards that are always connected)
+-- function setDefaultAudio()
+--   local current = hs.audiodevice.defaultOutputDevice()
+--   local usbAudio = findOutputByPartialUID(usbAudioSearch)
+--
+--   if usbAudio and current:name() ~= usbAudio:name() then
+--     usbAudio:setDefaultOutputDevice()
+--   end
+--
+--   hs.notify.new({
+--     title='Hammerspoon',
+--     informativeText='Default output device:' .. hs.audiodevice.defaultOutputDevice():name()
+--   }):send()
+-- end
+--
+-- function findOutputByPartialUID(uid)
+--   for _, device in pairs(hs.audiodevice.allOutputDevices()) do
+--     if device:uid():match(uid) then
+--       return device
+--     end
+--   end
+-- end
+--
+-- hs.usb.watcher.new(setDefaultAudio):start()
+-- setDefaultAudio()
