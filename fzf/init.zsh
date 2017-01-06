550c8c25f6db753c4f45a4fe1cc1b2eb3895cda7
Seth Voltz <svoltz@salesforce.com>
2017-01-06 14:14:27 -0800

    feat(fzf): Add new fzf topic

---
+++ fzf/init.zsh
@@ -0,0 +1,6 @@
+export FZF_DEFAULT_COMMAND='
+  (git ls-tree -r --name-only HEAD ||
+   find . -path "*/\.*" -prune -o -type f -print -o -type l -print |
+      sed s/^..//) 2> /dev/null'
+
+export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
