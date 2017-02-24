a9a1bbcb497c8bbd558f205b36bcd77067242379
Seth Voltz <svoltz@salesforce.com>
2017-02-24 13:54:34 -0800

    fix(ZSH): Remove Exa dependence as it no longer works on Sierra

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,7 +1,9 @@
 alias reload!='. ~/.zshrc'
 
-alias ls="exa -a"
-alias ll="exa -algh"
+# alias ls="exa -a"
+# alias ll="exa -algh"
+alias ls="ls -a"
+alias ll="ls -algh"
 alias ducks='du -cksh * | sort -rn | head -11' # Lists the size of all the folders and files
 alias top='top -o cpu'
 alias cd..="cd .."
