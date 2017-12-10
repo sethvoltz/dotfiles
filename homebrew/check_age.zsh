5b5e88b339f4182aa48a76401e316fa2e1f16dd9
Seth Voltz <seth@designgods.net>
2017-12-10 10:31:06 -0800

    fix(Homebrew): Disable homebrew age check/update

--- homebrew/check_age.zsh
+++ homebrew/check_age.zsh
@@ -1,10 +1,10 @@
-if [ $(which brew) ]; then
-  brew_repo=$(brew --repository)
-  let head_age=`/usr/bin/stat -f "%m" $brew_repo/.git/FETCH_HEAD 2> /dev/null || echo 0`
-  let repo_age=`date +"%s"`-$head_age
-  let grace_period="86400" # 86400 = 24 hours
-  if [ "$repo_age" -gt "$grace_period" ]; then
-    echo "Homebrew out of date (older than $grace_period seconds)... updating"
-    brew update
-  fi
-fi
+# if [ $(which brew) ]; then
+#   brew_repo=$(brew --repository)
+#   let head_age=`/usr/bin/stat -f "%m" $brew_repo/.git/FETCH_HEAD 2> /dev/null || echo 0`
+#   let repo_age=`date +"%s"`-$head_age
+#   let grace_period="86400" # 86400 = 24 hours
+#   if [ "$repo_age" -gt "$grace_period" ]; then
+#     echo "Homebrew out of date (older than $grace_period seconds)... updating"
+#     brew update
+#   fi
+# fi
