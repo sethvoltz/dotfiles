62a9c802862933fcd1458f481c6747e41b91e142
Seth Voltz <seth@designgods.net>
2019-09-03 12:53:12 -0700

    feat(Serverless): add serverless topic with autocompletion setup from zshrc

---
+++ serverless/serverless.zsh
@@ -0,0 +1,7 @@
+# tabtab source for serverless package
+# uninstall by removing these lines or running `tabtab uninstall serverless`
+[[ -f /Users/seth/.config/yarn/global/node_modules/tabtab/.completions/serverless.zsh ]] && . /Users/seth/.config/yarn/global/node_modules/tabtab/.completions/serverless.zsh
+
+# tabtab source for sls package
+# uninstall by removing these lines or running `tabtab uninstall sls`
+[[ -f /Users/seth/.config/yarn/global/node_modules/tabtab/.completions/sls.zsh ]] && . /Users/seth/.config/yarn/global/node_modules/tabtab/.completions/sls.zsh
