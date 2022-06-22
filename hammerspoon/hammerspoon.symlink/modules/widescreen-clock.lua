5ebaf81813c567f142febafec62334f6ea098d8e
Seth Voltz <svoltz@box.com>
2022-06-22 12:34:11 -0700

    feat(hammerspoon): add new menubar clock in the center of wide screens

---
+++ hammerspoon/hammerspoon.symlink/modules/widescreen-clock.lua
@@ -0,0 +1,78 @@
+local widescreenIndicators = {}
+local indicatorColor = hs.drawing.color.asRGB({ red = 1.0, green = 1.0, blue = 1.0, alpha = 1.0 })
+
+local clockStyle = {
+  font = {
+      size = 13
+  },
+  paragraphStyle = {
+      alignment = "center",
+  },
+  color = indicatorColor,
+}
+
+-- -------------------------------------------------------= Change Handlers =--=
+
+function updateWideClocks()
+  clearWideIndicators()
+
+  for _, screen in pairs(hs.screen.allScreens()) do
+    drawScreenClock(screen)
+  end
+end
+
+-- ------------------------------------------------------------= Indicators =--=
+
+function drawScreenClock(screen)
+  local screeng = screen:fullFrame()
+  local menubarHeight = screen:frame().y - screeng.y - 1
+
+  local screenRatio = screeng.w / screeng.h
+  if screenRatio < 2.5 then return end
+
+  local fontInfo = hs.styledtext.fontInfo(clockStyle.font)
+  local clockOffset = math.floor(menubarHeight - fontInfo.capHeight) / 2 - (fontInfo.ascender - fontInfo.capHeight)
+
+  local clock = hs.styledtext.new(os.date("%a %b %d %H:%M"), clockStyle)
+  local width = 250
+  local height = menubarHeight
+
+  wideIndicator = hs.canvas.new{
+    x = (screeng.x + screeng.w) / 2 - width / 2,
+    y = screeng.y,
+    w = width,
+    h = height,
+  }:appendElements(
+    {
+      action = "build",
+      type = "text",
+      text = clock,
+      frame = {
+        x = 0,
+        y = clockOffset,
+        w = width,
+        h = height - clockOffset
+      }
+    }
+  )
+  
+  wideIndicator
+    :level(hs.canvas.windowLevels.cursor)
+    :behavior(hs.canvas.windowBehaviors.canJoinAllSpaces)
+    :show()
+  
+  table.insert(widescreenIndicators, wideIndicator)
+end
+
+function clearWideIndicators()
+   for key, wideIndicator in pairs(widescreenIndicators) do
+      wideIndicator:delete()
+      widescreenIndicators[key] = nil
+   end
+end
+
+-- --------------------------------------------------------------= Watchers =--=
+
+_wideClockSpaceWatcher = hs.spaces.watcher.new(updateWideClocks):start()
+_wideClockTimer        = hs.timer.new(hs.timer.seconds(15), updateWideClocks):start()
+updateWideClocks()
