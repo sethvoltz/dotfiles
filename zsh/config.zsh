6910aff074a3f162f1edd64315257efc991ddd2d
Seth Voltz <seth@ascend.io>
2023-10-13 11:54:45 -0700

    feat(zsh): remove = from WORDCHARS so word-delete stops on them

--- zsh/config.zsh
+++ zsh/config.zsh
@@ -73,7 +73,7 @@ bindkey '^?' backward-delete-char
 # Kudos: https://github.com/cehoffman/dotfiles/blob/cec090cba571074ee77b0cc46f7f821c3ecaa988/zsh/bindings#L105-L110
 # Make it easy to remove directory components when deleting words
 function slash-backward-kill-word() {
-  local WORDCHARS="${WORDCHARS:s#/#}"
+  local WORDCHARS="${${WORDCHARS:s#/#}:s/=/}"
   zle backward-delete-word
 }
 zle -N slash-backward-kill-word
