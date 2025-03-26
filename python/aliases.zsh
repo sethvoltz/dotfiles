980450e9579a244dc7953553a30d4295750780fc
Seth Voltz <seth@ascend.io>
2025-03-26 13:28:44 -0700

    feat(python): add UV installer and helpful aliases

---
+++ python/aliases.zsh
@@ -0,0 +1,3 @@
+alias venv="uv venv"
+alias py-on=". .venv/bin/activate"
+alias py-off="deactivate"
---
+++ python/install.sh
@@ -0,0 +1,4 @@
+if ! command -v uv > /dev/null 2>&1; then
+  echo "» Installing uv"
+  curl -LsSf https://astral.sh/uv/install.sh | sh
+fi
