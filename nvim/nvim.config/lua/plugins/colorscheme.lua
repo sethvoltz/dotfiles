09d2fd9c6f06f7ae7dba7199695e16a4ae48cca0
Seth Voltz <seth@ascend.io>
2025-03-31 14:15:43 -0700

    feat(nvim): fix file typo and make theme transparent

--- nvim/nvim.config/lua/plugins/colorscheme.lua
+++ nvim/nvim.config/lua/plugins/colorscheme.lua
@@ -1,9 +1,19 @@
 return {
-  { "projekt0n/github-nvim-theme" },
+  {
+    "projekt0n/github-nvim-theme",
+    name = "github-theme",
+    config = function()
+      require("github-theme").setup({
+        options = {
+          transparent = true,
+        },
+      })
+    end,
+  },
   {
     "LazyVim/LazyVim",
     opts = {
       colorscheme = "github_dark_default",
-    }
+    },
   },
 }
