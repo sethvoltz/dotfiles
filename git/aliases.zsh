5a1a9401b4423dc1c40fffc24159404cdfa7dfbc
Seth Voltz <seth@ascend.io>
2025-10-15 12:55:02 -0700

    feat(git): add fzf-based wrappers to switch and delete branches

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -13,7 +13,7 @@ alias gd='git diff' # when using git-delta or diff-so-fancy
 # Repo & branch lifecycle
 alias gco='git checkout'
 # alias gb='git branch'
-alias gw='git switch'
+alias gw='git-switch-fzf'
 alias gwd='gw `default-branch`' # See zsh/functions/default-branch
 alias gn='gw -c'
 # alias gs='git status -sb' # upgrade your git if -sb breaks for you. it's fun.
